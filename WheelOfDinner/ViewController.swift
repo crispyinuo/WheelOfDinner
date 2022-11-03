062b910e293e11e051568b44dd2add2a4624ccf3
Zoe Zhou <zoez@usc.edu>
2022-11-02 18:22:43 -0700

    Initial Commit

---
+++ WheelOfDinner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WheelOfDinner
+//
+//  Created by Yinuo Zhou on 11/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WheelOfDinnerUITests/WheelOfDinnerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  WheelOfDinnerUITestsLaunchTests.swift
+//  WheelOfDinnerUITests
+//
+//  Created by Yinuo Zhou on 11/2/22.
+//
+
+import XCTest
+
+final class WheelOfDinnerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
